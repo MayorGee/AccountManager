406edaadeb4928c925155307e400d2c130f6a8ce
MayorGit <amayowa@lachestry.com>
2023-02-07 23:14:43 +0300

    Set up starterkit
    [master]

---
+++ src/components/Customer.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="customer">
+        <div class="customer__top">
+            Customer name:
+
+            {{ firstname }}
+        </div>
+
+        <div class="customer__bottom">
+            Customer surname:
+
+            {{ surname }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Customer',
+
+        props: {
+            firstname: {
+                default: '',
+                require: true,
+                type: String
+            },
+            surname: {
+                default: false,
+                require: true,
+                type: String
+            }
+        },
+
+        computed: {
+            fullname() {
+                return this.firstname + ' ' + this.surname;
+            }
+        },
+
+        watch: {
+            fullname(fullname) {
+                this.$emit('fullname-changed', fullname);
+            },
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .customer {
+        margin-top: 20px;
+        margin-bottom: 20px;
+    }
+</style>
---
+++ src/pages/App.vue
@@ -0,0 +1,99 @@
+<template>
+    <div>
+        <div
+            class="welcome-class"
+            :class="counter > 0 ? 'welcome-class_high' : 'welcome-class_low'"
+        >
+            <h2>{{ welcomeText }}</h2>
+        </div>
+
+        <input v-model="welcomeText" />
+
+        <input v-model="counter" />
+
+        <button @click="minus">
+            -
+        </button>
+
+        <button @click="plus">
+            +
+        </button>
+
+        <Customer
+            v-for="{ firstname, surname } in customers"
+            :firstname="firstname"
+            :surname="surname"
+            :key="`Customer ${firstname}`"
+        />
+    </div>
+</template>
+
+<script>
+    import Customer from '../components/Customer.vue';
+
+    export default {
+        name: 'HelloWebpack',
+
+        components: {
+            Customer
+        },
+
+        data() {
+            return {
+                welcomeText: 'Vue Lecture',
+                counter: 0,
+                customers: [
+                    {
+                        firstname: 'Sergey',
+                        surname: 'Darnopykh'
+                    },
+                    {
+                        firstname: 'Vlad',
+                        surname: 'Slesarenko'
+                    }
+                ]
+            }
+        },
+
+        methods: {
+            minus() {
+                this.counter -= 1;
+            },
+
+            plus() {
+                this.counter += 1;
+            },
+
+            onFullNameChange(fullname) {
+                console.log('fullname', fullname);
+            }
+        },
+
+        mounted() {},
+
+        created() {
+        }
+    }
+</script>
+
+<style scoped lang="scss">
+    .welcome-class {
+        h2 {
+            margin-top: 0;
+            margin-bottom: 2em;
+        }
+
+        padding: 1em;
+        border-radius: 4px;
+        font-size: 24px;
+        text-align: center;
+
+        &_low {
+            //background: antiquewhite;
+        }
+
+        &_high {
+            //background: aqua;
+        }
+    }
+</style>
